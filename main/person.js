d74fb203db94d82316a6f21e5d3f75077f08d0e6
giraffecat <1018997102@qq.com>
2017-06-01 20:01:00 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,2 +1,9 @@
 // Write your code here
 module.exports = Person
+{
+  var person={name:'王雪',age:21};
+  function introduce()
+  {  console.log('my name is 王雪，I am 21 years old');
+  }
+  introduce();
+}
