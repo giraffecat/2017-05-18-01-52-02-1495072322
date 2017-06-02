b88daf3d82982c3905f3b8b8d7db58cf6f30b6b7
giraffecat <1018997102@qq.com>
2017-06-02 15:36:13 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,9 +1,4 @@
-var Person = function(){	
-	this.name = "liyatang";
-};
-Person.prototype = {
-	//可以在这里提供Person的基本功能
-	getName : function(){
-		return this.name;
-	}
-}
+var Robot = new Object();
+Robot.name = "DeepSky";
+Robot.birthday = "1970年1月1日";
+Robot.say_hello = function(){console.log("我的名字是DeepSky,是一个出生在1970年1月1日的机器人.");}
