90ba64c9de2d817975c4311978f1fc1ac7a69131
giraffecat <1018997102@qq.com>
2017-06-02 09:18:45 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,10 +1,18 @@
 
-
-var person={name:'王雪',age:21};
-  function introduce()
-  {  console.log('my name is'+person.name+'I am'+person.age+'years old');
-  }
-  introduce();
-
-
+public class Manager extends Employee {  
+  private String department;  
+  public Manager(String name, double salary, String dept)  
+ {  
+   super(name, salary);  
+   department = dept;  
+ }  
+ public Manager(String n, String dept) {  
+   super(name);  
+   department = dept;  
+ }  
+ public Manager(String dept) {  
+  // 这里就没有super()，编译器会自动地添加一个空参数的缺省super构造器，此时如果Employee类中没有空参数的缺省构造器，那就会导致一个编译错误  
+   department = d;  
+ }  
+ }  
 
