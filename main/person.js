572f2f655535e297ec68e59c6c43fb808c7f5d59
giraffecat <1018997102@qq.com>
2017-06-02 15:13:53 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,32 +1,9 @@
-public class Student
-{
-private String name;
-private String gender;
-private int age;
-public Student()
-{
-}
-public Student(String name, String gender, int age)
-{
-this.name = name;
-this.gender = gender;
-this.age = age;
-}
-public String introduce()
-{
-return "My name is " + name + ". I am a " + gender + ", and my age is " + age + ".";
-}
-public String write(String content)
-{
-System.out.println("这书写的内容：" + content);
-return content;
-}
-public static void main(String []args)
-{
-Student s=new Student();
-Student s1=new Student("xinxin","female",15);
-s.introduce();
-s1.introduce();
-s.write("gone with the wind");
-}
+var Person = function(){	
+	this.name = "liyatang";
+};
+Person.prototype = {
+	//可以在这里提供Person的基本功能
+	getName : function(){
+		return this.name;
+	}
 }
