43ce91b62cdef611e5cd94a595de850a2fb63d60
giraffecat <1018997102@qq.com>
2017-06-02 08:51:00 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,6 +1,6 @@
 
-
-  var person={name:'王雪',age:21};
+class person{
+  var person={name:'王雪',age:21};}
   function introduce()
   {  console.log('my name is'+person.name+'I am'+person.age+'years old');
   }
