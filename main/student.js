5c18cc80d9df82820815abe3f9686fa9fc4e7afb
giraffecat <1018997102@qq.com>
2017-06-04 12:04:32 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,6 +1,5 @@
-var Robot = new Object();
-Robot.name = "DeepSky";
-Robot.birthday = "1970年1月1日";
-Robot.class="4";
-Robot.say_hello = function(){console.log("我的名字是DeepSky,是一个出生在1970年1月1日的机器人.来自4班");}
 
+let Person = require('./person')
+person.klss='4';
+person.introduce()
+module.exports = Student
