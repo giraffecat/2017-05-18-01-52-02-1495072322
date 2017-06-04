6a01bdf201c04f0fb90837e29fea2fd18aad7df6
giraffecat <1018997102@qq.com>
2017-06-04 11:36:27 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -2,7 +2,7 @@ function person(name,age){
   this.name=name;
   this.age=age;
   this.introduce=function(){
-    console.log('My name is '+this.name+',My age is '+this.age+' years old!')}
+    console.log('My name is '+this.name+',I am '+this.age+' years old!')}
 }
 
 
