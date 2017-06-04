1b275ee45b35c58a715bfb0d76ea4fed6acc27f6
giraffecat <1018997102@qq.com>
2017-06-04 12:34:35 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -5,7 +5,7 @@ this.name=name;
 this.age=age;
 this.klass=klass;
 this.introduce=function(){console.log('my name is'+Student.name+'my age is'+Student.age+'my klass is'+Student.klass)}
-    }
+   return(name,age,klass) }
 var Student=new Student('pill','21','4');
 Student.introduce();
 
