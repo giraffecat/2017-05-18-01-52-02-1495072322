2d30b0687afeca14e6783ddc1f59456ae90e3643
giraffecat <1018997102@qq.com>
2017-06-11 22:40:31 +0800

    Create person.js

--- main/person.js
+++ main/person.js
@@ -1,8 +1,6 @@
 function Person(name,age){
   this.name=name;
   this.age=age;
-  this.introduce=function(){
-    console.log('My name is'+this.name+' I am '+this.age+' years old.!')
-  this.introduce();}
+ alert('My name is'+this.name+' I am '+this.age+' years old.!')
 }
 module.exports = Person
