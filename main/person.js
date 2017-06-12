5d003164b55ee72c4facea7ad3a82d20bcf72fe0
giraffecat <1018997102@qq.com>
2017-06-12 23:24:01 +0800

    Create person.js

--- main/person.js
+++ main/person.js
@@ -2,7 +2,7 @@ function person(name,age){
   this.name=name;
   this.age=age;}
 person.prototype.introduce=function(){
-  return '('my name is'+this.name+'my age is'+this.age)'
+  console.log('my name is'+this.name+'my age is'+this.age);
 };
 module.exports = person
 
