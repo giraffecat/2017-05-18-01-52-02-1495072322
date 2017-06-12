ec873764fe5077b3e7099723a4bc274088d9550d
giraffecat <1018997102@qq.com>
2017-06-12 22:38:04 +0800

    Create student.js

--- main/student.js
+++ main/student.js
@@ -1,12 +1,5 @@
+let Person = require('./person')
+var Student=new Person();
+Student.klass=4;
 
-
-function Student(name,age,klass){
-this.name=name;
-this.age=age;
-this.klass=klass;
-this.introduce=function(){console.log('my name is'+Student.name+'my age is'+Student.age+'my klass is'+Student.klass)}
-   return(name,age,klass) }
-var Student=new Student('pill','21','4');
-Student.introduce();
-
-
+module.exports = Student
