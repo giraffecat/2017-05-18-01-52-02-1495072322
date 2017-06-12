ba716b4a6cd5c594613f6746c6db2e7df40351d0
giraffecat <1018997102@qq.com>
2017-06-12 22:27:32 +0800

    Create person.js

--- main/person.js
+++ main/person.js
@@ -1,7 +1,8 @@
 function person(kame,age){
   this.kame=kame;
   this.age=age;
-    console.log('My name is '+this.kame+'. I am '+this.age+' years old.');
+  this.introduce=function(){
+    console.log('My name is '+this.kame+'. I am '+this.age+' years old.');}
 }
 module.exports = person
 
