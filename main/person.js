298ba0166b45bc409d8bde173145aef0f7d738f7
giraffecat <1018997102@qq.com>
2017-06-18 19:14:14 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,8 +1,10 @@
-function person(name,age){
-  this.name=name;
-  this.age=age;}
-person.introduce=function(){
-  console.log('my name is'+this.name+'my age is'+this.age);
+class person{
+  constructor(name,age){
+    this.name=name;
+    this.age=age;}
+  person.introduce=function(){
+    console.log("my name is"+this.name+"  my age is "+this.age);
+  }
 }
 module.exports = person
 
