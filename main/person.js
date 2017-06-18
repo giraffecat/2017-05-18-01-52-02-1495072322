d0272cf479aec9a83334f76320f9f77273f8ea69
giraffecat <1018997102@qq.com>
2017-06-18 19:43:59 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -2,7 +2,7 @@ class person{
   constructor(name,age){
     this.name=name;
     this.age=age;
-    return "my name is"+this.name+"my age is"+this.age;
+    return "my name is"+this.name+"my age is"+this.age
   }
 }
 module.exports = person
